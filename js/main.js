f5aeeb0037b19e4aeaf987cba7f303215ff5ced9
Anna Wrzosek <annecreeyates@gmail.com>
2025-08-12 20:53:58 +0100

    PWM2-1 | Removed React from the project

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Main JavaScript file
+document.addEventListener('DOMContentLoaded', () => {
+    // Your JavaScript code will go here
+});
+
+// You can split your JavaScript into multiple files based on features
+// For example: about.js, projects.js, contact.js
