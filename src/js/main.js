4159c86832b79e6f0d506d07ebb722956910b452
Anna Wrzosek <annecreeyates@gmail.com>
2025-12-02 19:23:56 +0000

    PWM2-1 | Removed lava-lamp

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,3 @@
 import '../css/styles.css'
-import LavaLamp from './lava-lamp.js'
 
-document.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.querySelector('.lava-lamp-canvas');
-    if (canvas) {
-        new LavaLamp(canvas);
-    }
-});
 
