bab296ed68a1910123a8d216fbe7bf61dbe614eb
Anna Wrzosek <annecreeyates@gmail.com>
2026-02-04 22:31:43 +0000

    PWM2-2 | Added the reusability for the card component

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,11 +1,15 @@
 import "./card.css";
 
-export function createCard() {
-  const cardContainer = document.getElementById("project-card-1");
+export function createCard({ title, description, repoLink, livePageLink }) {
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-  cardContainer.innerHTML = `
-    <div class="card">
-      <p>Project Title</p>
-    </div>
+  card.innerHTML = `
+      <h3>${title}</h3>
+      <p>${description}</p>
+      <button><a href="${repoLink}" target="_blank">View Repo</a></button>
+      <button><a href="${livePageLink}" target="_blank">Live Page</a></button>
   `;
+
+  return card;
 }
--- src/main.js
+++ src/main.js
@@ -3,4 +3,23 @@ import "./components/header/header.js";
 
 import { createCard } from "./components/card/card.js";
 
-createCard();
+const projectCard = document.getElementById("project-cards");
+
+const card1 = createCard({
+  title: "Project One",
+  description: "Description here",
+  repoLink: "#",
+  livePageLink: "#",
+});
+
+const card2 = createCard({
+  title: "Project Two",
+  description: "Description here",
+  repoLink: "#",
+  livePageLink: "#",
+});
+
+if (projectCard) {
+  projectCard.appendChild(card1);
+  projectCard.appendChild(card2);
+}
