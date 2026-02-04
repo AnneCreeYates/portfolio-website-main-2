f0c86fbacd113bbc98182cc002630382f4837a81
Anna Wrzosek <annecreeyates@gmail.com>
2026-02-04 23:26:29 +0000

    PWM2-2 | Fixed the issue with unsafe-dom-assignment

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,12 +4,27 @@ export function createCard({ title, description, repoLink, livePageLink }) {
   const card = document.createElement("div");
   card.classList.add("card");
 
-  card.innerHTML = `
-      <h3>${title}</h3>
-      <p>${description}</p>
-      <button><a href="${repoLink}" target="_blank">View Repo</a></button>
-      <button><a href="${livePageLink}" target="_blank">Live Page</a></button>
-  `;
+  const h3 = document.createElement("h3");
+  h3.textContent = title;
+
+  const p = document.createElement("p");
+  p.textContent = description;
+
+  card.append(h3, p);
+
+  if (repoLink) {
+    const repoAnchor = document.createElement("a");
+    repoAnchor.textContent = "GitHub";
+    repoAnchor.href = repoLink;
+    card.append(repoAnchor);
+  }
+
+  if (livePageLink) {
+    const livePageAnchor = document.createElement("a");
+    livePageAnchor.textContent = "Live Page";
+    livePageAnchor.href = livePageLink;
+    card.append(livePageAnchor);
+  }
 
   return card;
 }
